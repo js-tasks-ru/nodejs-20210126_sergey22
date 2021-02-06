0fe58e3b7e2a3f4ebff24fbee74ce41959005ad2
Serhii Paladi <sergey@paladi.od.ua>
2021-02-06 13:21:52 +0200

    add solution for 01-intro 01-sum

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,5 +1,10 @@
 function sum(a, b) {
-  /* ваш код */
+    if ((typeof (a) !== 'number') || (typeof (b) !== 'number')) {
+        throw new TypeError();
+    }
+
+    const sum = a + b;
+    return sum;
 }
 
 module.exports = sum;
