d9a4b0c1cfdc25e8fbf7953947e0943aac66ba0d
Serhii Paladi <sergey@paladi.od.ua>
2021-03-10 14:52:14 +0200

    add solution for o3-streams 02-line-split-stream

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -4,12 +4,31 @@ const os = require('os');
 class LineSplitStream extends stream.Transform {
   constructor(options) {
     super(options);
+    this.rSymbol = os.EOL;
+    this.finStr ='';
   }
 
   _transform(chunk, encoding, callback) {
+    this.finStr += chunk;// .toString();
+
+    const findSplit  = this.finStr.split(this.rSymbol);
+
+    for(var n = 0; n < findSplit.length-1; n++) {
+      this.push(findSplit[n]);
+    }
+
+   //  if (findSplit[n].toString().length>0){
+       this.finStr =findSplit[n++];
+   // }
+    callback();
   }
 
   _flush(callback) {
+    if (this.finStr) {
+      this.push(this.finStr);
+    }
+
+    callback();
   }
 }
 
